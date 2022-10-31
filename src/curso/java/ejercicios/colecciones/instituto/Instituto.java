5010d45c877135978157cc9bdfbf3037e56041fc
Alejandro Ruiz <a.ruiz.r98@gmail.com>
2022-10-31 11:32:31 +0100

    Añadido constructor para inicializar los estudiantes en lugar de inicializarlo en el método abrirInstituto

--- src/curso/java/ejercicios/colecciones/instituto/Instituto.java
+++ src/curso/java/ejercicios/colecciones/instituto/Instituto.java
@@ -8,90 +8,100 @@ import java.util.Set;
 
 public class Instituto {
 	private List<Estudiante> estudiantes;
+
+	public Instituto() {
+		this.estudiantes = new ArrayList<Estudiante>();
+	}
+
 	public void abrirInstituto() {
-		
-		Set<Asignatura> asignaturas1=new HashSet<Asignatura>();
+
+		Set<Asignatura> asignaturas1 = new HashSet<Asignatura>();
 		asignaturas1.add(new Asignatura("Lengua"));
 		asignaturas1.add(new Asignatura("Latín"));
 		asignaturas1.add(new Asignatura("Griego"));
-		Set<Asignatura> asignaturas2=new HashSet<Asignatura>();
+		Set<Asignatura> asignaturas2 = new HashSet<Asignatura>();
 		asignaturas2.add(new Asignatura("Matemáticas"));
 		asignaturas2.add(new Asignatura("Física"));
 		asignaturas2.add(new Asignatura("Química"));
-		Set<Asignatura> asignaturas3=new HashSet<Asignatura>();
+		Set<Asignatura> asignaturas3 = new HashSet<Asignatura>();
 		asignaturas3.add(new Asignatura("Lengua"));
 		asignaturas3.add(new Asignatura("Matemáticas"));
-		Set<Asignatura> asignaturas4=new HashSet<Asignatura>();
+		Set<Asignatura> asignaturas4 = new HashSet<Asignatura>();
 		asignaturas4.add(new Asignatura("Lengua"));
 		asignaturas4.add(new Asignatura("Latín"));
-		Set<Asignatura> asignaturas5=new HashSet<Asignatura>();
+		Set<Asignatura> asignaturas5 = new HashSet<Asignatura>();
 		asignaturas5.add(new Asignatura("Matemáticas"));
 		asignaturas5.add(new Asignatura("Física"));
-		
 
-		Estudiante e1=new Estudiante("Carlos", "García",asignaturas1);
-		Estudiante e2=new Estudiante("Juan", "Robles",asignaturas2);
-		Estudiante e3=new Estudiante("Sergio", "Rodriguez",asignaturas3);
-		Estudiante e4=new Estudiante("Pablo", "Sánchez",asignaturas4);
-		Estudiante e5=new Estudiante("Daniel", "López",asignaturas5);
-		
-		this.estudiantes=new ArrayList<Estudiante>();
+		Estudiante e1 = new Estudiante("Carlos", "García", asignaturas1);
+		Estudiante e2 = new Estudiante("Juan", "Robles", asignaturas2);
+		Estudiante e3 = new Estudiante("Sergio", "Rodriguez", asignaturas3);
+		Estudiante e4 = new Estudiante("Pablo", "Sánchez", asignaturas4);
+		Estudiante e5 = new Estudiante("Daniel", "López", asignaturas5);
+
 		this.estudiantes.add(e1);
 		this.estudiantes.add(e2);
 		this.estudiantes.add(e3);
 		this.estudiantes.add(e4);
-		this.estudiantes.add(e5);	
+		this.estudiantes.add(e5);
 	}
+
 	public void mostrarEstudiantes() {
 		System.out.println("Estudiantes:\n");
 		for (Estudiante estudiante : estudiantes) {
-			System.out.println(estudiante.getNombre()+" "+estudiante.getApellidos());
+			System.out.println(estudiante.getNombre() + " " + estudiante.getApellidos());
 			for (Asignatura asignatura : estudiante.getAsignaturas()) {
-				System.out.println(asignatura.getNombre()+": "+String.format("%.2f", asignatura.getNota()));
+				System.out.println(asignatura.getNombre() + ": " + String.format("%.2f", asignatura.getNota()));
 			}
 			System.out.println();
 		}
 	}
+
 	public void filtrarAlumnosPorAsignatura(String asignatura) {
-		System.out.println("Estudiantes que cursan "+asignatura+":");
+		System.out.println("Estudiantes que cursan " + asignatura + ":");
 		for (Estudiante estudiante : estudiantes) {
 			for (Asignatura a : estudiante.getAsignaturas()) {
-				if(a.getNombre().equals(asignatura)) {
-					System.out.println(estudiante.getNombre()+" "+estudiante.getApellidos());
+				if (a.getNombre().equals(asignatura)) {
+					System.out.println(estudiante.getNombre() + " " + estudiante.getApellidos());
 				}
 			}
 		}
 		System.out.println();
 	}
+
 	public void mostrarAprobados() {
 		System.out.println("Alumnos que han aprobado el curso:");
 		double media;
 		for (Estudiante estudiante : estudiantes) {
-			media=estudiante.calcularNotaMedia();
-			if(media >=5) {
-				System.out.println(estudiante.getNombre()+" "+estudiante.getApellidos()+". Nota media: "+String.format("%.2f", media));
+			media = estudiante.calcularNotaMedia();
+			if (media >= 5) {
+				System.out.println(estudiante.getNombre() + " " + estudiante.getApellidos() + ". Nota media: "
+						+ String.format("%.2f", media));
 			}
 		}
 		System.out.println();
 	}
+
 	public void eliminarEstudiantesSuspensos(String asignatura) {
-		boolean suspenso=false;
+		boolean suspenso = false;
 		for (Iterator iterator = estudiantes.iterator(); iterator.hasNext();) {
 			Estudiante estudiante = (Estudiante) iterator.next();
 			for (Iterator iterator2 = estudiante.getAsignaturas().iterator(); iterator2.hasNext();) {
 				Asignatura a = (Asignatura) iterator2.next();
-				if(a.getNombre().equals(asignatura) && a.getNota() < 5) {
-					System.out.println("El estudiante "+estudiante.getNombre()+" "+estudiante.getApellidos()+" ha sido eliminado porque ha suspendido "+asignatura+".");
+				if (a.getNombre().equals(asignatura) && a.getNota() < 5) {
+					System.out.println("El estudiante " + estudiante.getNombre() + " " + estudiante.getApellidos()
+							+ " ha sido eliminado porque ha suspendido " + asignatura + ".");
 					iterator.remove();
-					suspenso=true;
+					suspenso = true;
 				}
 			}
 		}
-		if(!suspenso) {
-			System.out.println("Todos los estudiantes han aprobado "+asignatura+".");
+		if (!suspenso) {
+			System.out.println("Todos los estudiantes han aprobado " + asignatura + ".");
 		}
 		System.out.println();
 	}
+
 	public static void main(String[] args) {
 		Instituto instituto = new Instituto();
 		instituto.abrirInstituto();
